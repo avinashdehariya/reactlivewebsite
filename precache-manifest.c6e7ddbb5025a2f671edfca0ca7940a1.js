58bf831193f3413612bc42269f0261651ef5e29f
avinashdehariya <avinasdehariyaji@gmail.com>
2021-12-06 15:41:41 +0530

    Updates

---
+++ precache-manifest.c6e7ddbb5025a2f671edfca0ca7940a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d90cc1c9dff49bf73a28541b0893bbd",
+    "url": "/avinashdehariya.io/production/index.html"
+  },
+  {
+    "revision": "622f476dbe83ef6ea4a0",
+    "url": "/avinashdehariya.io/production/static/css/main.f7c88852.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/avinashdehariya.io/production/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/avinashdehariya.io/production/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622f476dbe83ef6ea4a0",
+    "url": "/avinashdehariya.io/production/static/js/main.a7712db5.chunk.js"
+  },
+  {
+    "revision": "a8a23132407d99bd42d1",
+    "url": "/avinashdehariya.io/production/static/js/runtime-main.e7afda5f.js"
+  }
+]);
